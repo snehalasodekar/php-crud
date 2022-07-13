cde790d442ec9d52ce695b7727bf5e30fcdb3622
Snehal Asodekar <snehalchetanasodekar@gmail.com>
2022-07-13 16:29:35 +0200

    clean index file

--- index.php
+++ index.php
@@ -4,18 +4,7 @@ declare(strict_types=1);
 
 require('vendor/autoload.php');
 require 'Controller/HomePageController.php';
-require 'Controller/Cart.php';
 require 'Model/DbConnection.php';
-require 'Model/Customer.php';
-require 'Model/CustomerGroups.php';
-require 'Model/CustomerLoader.php';
-
-
-session_start();
-if (!isset($_SESSION)) {
-    $_SESSION["cart"] = [];
-}
-
 
 
 $controller = new HomePageController();
