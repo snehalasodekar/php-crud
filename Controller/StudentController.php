f9e5417675e301a5213cb538eff576eeb7efbbd4
shivamtg <shivamtg1376@gmail.com>
2022-07-14 14:29:43 +0200

    recieve an array of students in studentController

--- Controller/StudentController.php
+++ Controller/StudentController.php
@@ -2,5 +2,19 @@
 
 class StudentController
 {
+    private DbConnection $studentLoader;
+    public function __construct(DbConnection $conn)
+    {
+        $this->studentLoader = $conn;
+    }
+
+    public function render(){
+        $studentArr = $this->studentLoader->getAllStudents();
+        var_dump($studentArr);
+        foreach ($studentArr as $student){
+            var_dump($student->getTeachers());
+        }
+
+    }
 
 }
