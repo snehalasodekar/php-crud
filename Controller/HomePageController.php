1b1f72462812763f963f8bb32545a7e2c659820d
shivamtg <shivamtg1376@gmail.com>
2022-07-14 16:50:58 +0200

    add switch case to homePageController to navigate

--- Controller/HomePageController.php
+++ Controller/HomePageController.php
@@ -2,17 +2,29 @@
 
 class HomePageController
 {
-    public function render(){
+    public function render() : void{
 
-        /*$classRoomLoader = new ClassRoomLoader();
-        $controller = new ClassRoomController($classRoomLoader);
-        $controller->renderClassRoom();
-        $classRoomArray = $classRoomLoader->getAllClassRooms();*/
-
-        $studentLoader = new StudentLoader();
-        $controller = new StudentController($studentLoader);
-
-
-        $controller->render();
+        if (isset($_GET['page'])){
+            switch ($_GET['page']){
+                case 'teacher':
+                    $teacherLoader = new TeacherLoader();
+                    $controller = new TeacherController($teacherLoader);
+                    break;
+                case 'student':
+                    $studentLoader = new StudentLoader();
+                    $controller = new StudentController($studentLoader);
+                    break;
+                case 'class':
+                    $classRoomLoader = new ClassRoomLoader();
+                    $controller = new ClassRoomController($classRoomLoader);
+                    break;
+                default:
+                    $controller = new HomePageController();
+                    break;
+            }
+            $controller->render();
+        }else{
+            include_once 'View/homepageView.php';
+        }
     }
 }
