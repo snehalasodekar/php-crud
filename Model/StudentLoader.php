c84754c04d78eef78cf9fc00d454f5b79b9959fd
shivamtg <shivamtg1376@gmail.com>
2022-07-14 14:43:42 +0200

    fetch students and create object of each student

--- Model/StudentLoader.php
+++ Model/StudentLoader.php
@@ -2,13 +2,44 @@
 
 class StudentLoader extends DbConnection
 {
+    private array $studentArr = [];
+
     public function __construct()
     {
         parent::__construct();
     }
 
-    public static function getAllStudents(): array
+    public function getAllStudents(): array
     {
-       //$this->connect->query('select * from student');
+        $students = $this->connect->query('select * from student')->fetchAll();
+        foreach ($students as $student) {
+            $teachArr = $this->fetchTeacherForStdnt($student['id']);
+            $class = $this->fetchClassForStdnt($student['id']);
+            $stdntObj = new Student ($student['id'], $student['firstname'], $student['lastname'],
+            $student['email'], $student['address'], $class, $teachArr);
+            array_push($this->studentArr, $stdntObj);
+
+        }
+        return $this->studentArr;
+    }
+
+    private function fetchTeacherForStdnt(int $id) : array
+    {
+        $query = 'select teacher_table.firstname
+            from teacher_table , class_table , student
+            where student.class_id = class_table.id and teacher_table.class_id = class_table.id
+            and student.id =' . $id;
+        return $this->connect->query($query)->fetchAll();
+
+    }
+
+    private function fetchClassForStdnt(int $id) : string
+    {
+        $query = 'select class_table.name
+        from  class_table , student
+        where student.class_id = class_table.id
+        and student.id = ' . $id;
+        $class = $this->connect->query($query)->fetch();
+        return $class[0];
     }
 }
