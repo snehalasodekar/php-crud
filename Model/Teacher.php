b9760f49f79861b24e7b5037320fc3a179255248
Snehal Asodekar <snehalchetanasodekar@gmail.com>
2022-07-14 11:14:51 +0200

    add getter methods with constructor to teacher class

--- Model/Teacher.php
+++ Model/Teacher.php
@@ -2,5 +2,69 @@
 
 class Teacher
 {
-    private int $
+    private int $id;
+    private string $firstName;
+    private string $lasName;
+    private string $email;
+    private string $address;
+    private int $classId;
+
+    public function __construct()
+    {
+        $this->firstName = $firstName;
+        $this->lasName = $lastName;
+        $this->email = $email;
+        $this->address = $address;
+        $this->classId = $classId;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFirstName(): string
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLasName(): string
+    {
+        return $this->lasName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAddress(): string
+    {
+        return $this->address;
+    }
+
+    /**
+     * @return int
+     */
+    public function getClassId(): int
+    {
+        return $this->classId;
+    }
+
+
 }
