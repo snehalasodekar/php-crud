eb0c8c442f8e48f424ce24d11c9ea56943b76440
shivamtg <shivamtg1376@gmail.com>
2022-07-14 16:51:24 +0200

    add buttons to homePageView to navigate

--- View/homepageView.php
+++ View/homepageView.php
@@ -1,4 +1,14 @@
 <?php
-echo "Homepage View";
-
 ?>
+<form method="get">
+<div class="container">
+        <div class="row">
+            <div class="col-md-4 offset-md-4">
+                <button class="btn btn-outline-primary" name="page" value="student">The students</button>
+                <button class="btn btn-outline-info" name="page" value="teacher">The teachers</button>
+                <button class="btn btn-outline-secondary" name="page" value="class">The classes</button>
+            </div>
+        </div>
+</div>
+</form>
+
