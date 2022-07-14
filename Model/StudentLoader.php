1be7320825f78bff49381ec87e6c049c5e42febf
shivamtg <shivamtg1376@gmail.com>
2022-07-14 11:14:23 +0200

    add get all students function to student loader class

--- Model/StudentLoader.php
+++ Model/StudentLoader.php
@@ -2,8 +2,13 @@
 
 class StudentLoader extends DbConnection
 {
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
     public static function getAllStudents(): array
     {
-
+       //$this->connect->query('select * from student');
     }
 }
