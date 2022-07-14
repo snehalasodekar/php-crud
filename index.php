f0d77d8b41af594ac9f3f4b855f70cacd6be44c8
shivamtg <shivamtg1376@gmail.com>
2022-07-14 16:52:38 +0200

    modify index file

--- index.php
+++ index.php
@@ -10,9 +10,13 @@ require 'Controller/HomePageController.php';
 require 'Controller/ClassRoomController.php';
 require 'Controller/StudentController.php';
 
+
 require 'View/header.php';
 
 $controller = new HomePageController();
 $controller->render();
+
 require 'View/footer.php';
-?>
+
+
+
