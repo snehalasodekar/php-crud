6f33d9f8d48915cf27d62e95c54320b5dcb57dc9
shivamtg <shivamtg1376@gmail.com>
2022-07-14 11:11:45 +0200

    modify index loading in requirments

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 require('vendor/autoload.php');
 require 'Controller/HomePageController.php';
 require 'Model/DbConnection.php';
+require 'Model/StudentLoader.php';
 
 
 $controller = new HomePageController();
