2340f0b539a58ff2f2ac7e7ea32f49c4b0286064
Snehal Asodekar <snehalchetanasodekar@gmail.com>
2022-07-15 11:15:33 +0200

    remove teacher loader object code as it added by another dev to manage student,classroom and teacher data together

--- Controller/HomePageController.php
+++ Controller/HomePageController.php
@@ -3,8 +3,6 @@
 class HomePageController
 {
     public function render(){
-        $teacherLoaderObj = new TeacherLoader();
-        $teacherController = new TeacherController($teacherLoaderObj);
-        $teacherController->render();
+       
     }
 }
