f9c6b57f1d1f180ea57882a30e6c44d09b6b5822
Snehal Asodekar <snehalchetanasodekar@gmail.com>
2022-07-15 10:50:16 +0200

    define render method with when to display teacher view and teacher details view

--- Controller/TeacherController.php
+++ Controller/TeacherController.php
@@ -2,5 +2,23 @@
 
 class TeacherController
 {
-
+        private DbConnection $DbConTeacherLoader;
+        public function __construct(DbConnection $con)
+        {
+            $this->DbConTeacherLoader = $con;
+        }
+        public function render():void {
+           $teacherDataArr = $this->DbConTeacherLoader->getAllTeachers();
+           if(isset($_POST['detail'])){
+                $teacherId = (int)$_POST['detail'];
+                $teachrDetails = $teacherDataArr[$teacherId-1];
+                include 'View/teacherDetailView.php';
+           }else{
+               if(isset($_POST['detailReturn']) && $_POST['detailReturn'] =='fromdetailpage'){
+                   include 'View/teacherView.php';
+               } else{
+                   include 'View/teacherView.php';
+               }
+            }
+        }
 }
