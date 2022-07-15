bf447edc244829a87e6df391b22345508fa40e79
Snehal Asodekar <snehalchetanasodekar@gmail.com>
2022-07-15 14:56:40 +0200

    add switch statement for update teacher

--- Controller/TeacherController.php
+++ Controller/TeacherController.php
@@ -2,23 +2,34 @@
 
 class TeacherController
 {
-        private DbConnection $DbConTeacherLoader;
-        public function __construct(DbConnection $con)
-        {
-            $this->DbConTeacherLoader = $con;
-        }
-        public function render():void {
-           $teacherDataArr = $this->DbConTeacherLoader->getAllTeachers();
-           if(isset($_POST['detail'])){
-                $teacherId = (int)$_POST['detail'];
-                $teachrDetails = $teacherDataArr[$teacherId-1];
-                include 'View/teacherDetailView.php';
-           }else{
-               if(isset($_POST['detailReturn']) && $_POST['detailReturn'] =='fromdetailpage'){
-                   include 'View/teacherView.php';
-               } else{
-                   include 'View/teacherView.php';
-               }
+    private DbConnection $DbConTeacherLoader;
+
+    public function __construct(DbConnection $con)
+    {
+        $this->DbConTeacherLoader = $con;
+    }
+
+    public function render(): void
+    {
+        $teacherDataArr = $this->DbConTeacherLoader->getAllTeachers();
+        if (isset($_POST['detail'])) {
+            $teacherId = (int)$_POST['detail'];
+            $teacherDetails = $teacherDataArr[$teacherId - 1];
+            include 'View/teacherDetailView.php';
+        } else {
+            if (isset($_POST['detailReturn'])) {
+                $page = $_POST['detailReturn'];
+                switch ($page) {
+                    case 'fromdetailpage':
+                        include 'View/teacherView.php';
+                        break;
+                    case 'updateTeacher' :
+                        include 'View/teacherUpdateView.php';
+                        break;
+                }
+            } else {
+                include 'View/teacherView.php';
             }
         }
+    }
 }
