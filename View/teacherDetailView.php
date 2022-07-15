4451bc1bc2ce58d4ed2fd190bdf2801277617462
Snehal Asodekar <snehalchetanasodekar@gmail.com>
2022-07-15 14:58:57 +0200

    add Update button

--- View/teacherDetailView.php
+++ View/teacherDetailView.php
@@ -1,34 +1,43 @@
 <table class="table">
     <thead>
     <tr>
+        <th scope="col">Id</th>
         <th scope="col">First Name</th>
         <th scope="col">Last Name</th>
         <th scope="col">Email</th>
         <th scope="col">Address</th>
         <th scope="col">Class</th>
         <th scope="col">Students</th>
+        <th scope="col">Actions</th>
 
     </tr>
     </thead>
     <tbody>
     <tr>
         <?php //$teachrDetails get from controller?>
-       <th scope="col"><?= $teachrDetails->getId() ?></th>
-        <th scope="col"><?= $teachrDetails->getFirstName() ?></th>
-        <th scope="col"><?= $teachrDetails->getLastName() ?></th>
-        <th scope="col"><?= $teachrDetails->getEmail() ?></th>
-        <th scope="col"><?= $teachrDetails->getAddress() ?></th>
-        <th scope="col"><?php echo  implode(",",$teachrDetails->getClassName()); ?></th>
-        <th scope="col"><?php echo implode(",",$teachrDetails->getStudents()); ?></th>
-        <th scope="col"><?php
-/*            echo implode(',',$foundStudent->getTeachers()); */?></th><!-- -->
+       <td><?= $teacherDetails->getId() ?></td>
+        <td><?= $teacherDetails->getFirstName() ?></td>
+        <td><?= $teacherDetails->getLastName() ?></td>
+        <td><?= $teacherDetails->getEmail() ?></td>
+        <td><?= $teacherDetails->getAddress() ?></td>
+        <td><?php echo  implode(",",$teacherDetails->getClassName()); ?></td>
+        <td><?php echo implode(",",$teacherDetails->getStudents()); ?></td>
+
+            <td>
+                <form method="post">
+                    <input type="hidden" name="teacherId" value="<?= $teacherDetails->getId(); ?>">
+                <button type="submit" name="detailReturn" class="btn btn-success"
+                        value="updateTeacher">Update</button>
+                </form>
+            </td>
+
     </tr>
     <tr>
         <form method="post">
-           <div>
+           <td>
                <button type="submit" name="detailReturn" class="btn btn-success"
                        value="fromdetailpage">Go Back</button>
-           </div>
+           </td>
         </form>
     </tr>
     </tbody>
