0b2f05cca2341f0c203910b590cf9dcc9175315d
Snehal Asodekar <snehalchetanasodekar@gmail.com>
2022-07-15 11:11:54 +0200

    Dispaly all deatails of particular teacher with classroom assigned to teacher and students of teacher

--- View/teacherDetailView.php
+++ View/teacherDetailView.php
@@ -1,1 +1,35 @@
-<?php
+<table class="table">
+    <thead>
+    <tr>
+        <th scope="col">First Name</th>
+        <th scope="col">Last Name</th>
+        <th scope="col">Email</th>
+        <th scope="col">Address</th>
+        <th scope="col">Class</th>
+        <th scope="col">Students</th>
+
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <?php //$teachrDetails get from controller?>
+       <th scope="col"><?= $teachrDetails->getId() ?></th>
+        <th scope="col"><?= $teachrDetails->getFirstName() ?></th>
+        <th scope="col"><?= $teachrDetails->getLastName() ?></th>
+        <th scope="col"><?= $teachrDetails->getEmail() ?></th>
+        <th scope="col"><?= $teachrDetails->getAddress() ?></th>
+        <th scope="col"><?php echo  implode(",",$teachrDetails->getClassName()); ?></th>
+        <th scope="col"><?php echo implode(",",$teachrDetails->getStudents()); ?></th>
+        <th scope="col"><?php
+/*            echo implode(',',$foundStudent->getTeachers()); */?></th><!-- -->
+    </tr>
+    <tr>
+        <form method="post">
+           <div>
+               <button type="submit" name="detailReturn" class="btn btn-success"
+                       value="fromdetailpage">Go Back</button>
+           </div>
+        </form>
+    </tr>
+    </tbody>
+</table>
