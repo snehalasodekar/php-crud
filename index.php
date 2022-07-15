0b25c6fce03f4ae18a77387aba04054f6d525350
Snehal Asodekar <snehalchetanasodekar@gmail.com>
2022-07-15 11:13:53 +0200

    add Model and controller files reference to index file

--- index.php
+++ index.php
@@ -3,9 +3,11 @@ declare(strict_types=1);
 
 require 'Model/DbConnection.php';
 require 'Model/ClassRoomLoader.php';
-require('vendor/autoload.php');
+require 'Model/TeacherLoader.php';
+require 'Model/Teacher.php';
+require 'vendor/autoload.php';
 require 'Controller/HomePageController.php';
-
+require 'Controller/TeacherController.php';
 require 'Controller/ClassRoomController.php';
 
 
