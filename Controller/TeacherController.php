0bd81d6e4ee1b7d60e31889f260f3eae0b8007ed
Snehal Asodekar <snehalchetanasodekar@gmail.com>
2022-07-16 13:47:37 +0200

    using switch CRUD Model methods and views of teacher calls

--- Controller/TeacherController.php
+++ Controller/TeacherController.php
@@ -11,25 +11,73 @@ class TeacherController
 
     public function render(): void
     {
-        $teacherDataArr = $this->DbConTeacherLoader->getAllTeachers();
-        if (isset($_POST['detail'])) {
-            $teacherId = (int)$_POST['detail'];
-            $teacherDetails = $teacherDataArr[$teacherId - 1];
-            include 'View/teacherDetailView.php';
-        } else {
-            if (isset($_POST['detailReturn'])) {
-                $page = $_POST['detailReturn'];
-                switch ($page) {
-                    case 'fromdetailpage':
-                        include 'View/teacherView.php';
-                        break;
-                    case 'updateTeacher' :
-                        include 'View/teacherUpdateView.php';
-                        break;
-                }
-            } else {
-                include 'View/teacherView.php';
+        //$teacherDataArr = $this->DbConTeacherLoader->getAllTeachers();
+        if (isset($_POST)) {
+            $submit = 'default';
+            $submit = $_POST['page'];
+            switch ($submit) {
+                /*case 'teacher':
+                    $teacherDataArr = $this->DbConTeacherLoader->getAllTeachers();
+                    include_once 'View/teacherView.php';
+                    break;*/
+                case 'detailTeacher': //show all teachers READ
+                    $teacherId = (int)$_POST['teacherIdHidden'];
+                    $teacherDetails = $this->DbConTeacherLoader->getTeacherById($teacherId);
+                    $tescherStudents = $this->DbConTeacherLoader->getStudentsForTeacher($teacherId);
+                    $tescherClasses = $this->DbConTeacherLoader->getClassesForTeacher($teacherId);
+                    include 'View/teacher/teacherDetailView.php';
+                    break;
+                case 'detailReturn': //go back button
+                    $teacherDataArr = $this->DbConTeacherLoader->getAllTeachers();
+                    include_once 'View/teacherView.php';
+                    break;
+                case 'deleteTeacher': // Delete Teacher DELETE
+                    $teacherId = (int)$_POST['teacherIdHidden'];
+                    $this->DbConTeacherLoader->deleteTeacherbyId($teacherId);
+                    $teacherDataArr = $this->DbConTeacherLoader->getAllTeachers();
+                    include_once 'View/teacherView.php';
+                    break;
+                case 'createTeacher': //show create teacher form
+                    $classData = $this->DbConTeacherLoader->getAllClassIdNames();
+                    include_once 'View/teacher/teacherCreateView.php';
+                    break;
+                case 'newTeacher': // Create a new teacher query and display updated view
+                    $classId = (int)$_POST['classroom'];
+                    $newTeacherData = $this->DbConTeacherLoader
+                        ->createNewTeacher(strtolower($_POST['first_name']), strtolower($_POST['last_name']), strtolower($_POST['email']), strtolower($_POST['address']), $classId);
+                    $teacherDataArr = $this->DbConTeacherLoader->getAllTeachers();
+                    include_once 'View/teacherView.php';
+                    break;
+                case 'updateTeacherForm' : //show update teacher form
+                    $teacherId = (int)$_POST['teacherIdHidden'];
+                    $teacherDetails = $this->DbConTeacherLoader->getTeacherById($teacherId);
+                    $classData = $this->DbConTeacherLoader->getAllClassIdNames();
+                    include_once 'View/teacher/teacherUpdateView.php';
+                    break;
+                case 'updateTeacherQuery': // Update a teacher UPDATE
+                    $classId = (int)$_POST['classroom'];
+                    $teacherId = (int)$_POST['teacherIdHidden'];
+                    //$teacherDataArr = $this->DbConTeacherLoader->getAllTeachers();
+                    $updateTeacher = $this->DbConTeacherLoader->
+                    updateTeacherId($teacherId,strtolower($_POST['first_name']), strtolower($_POST['last_name']), strtolower($_POST['email']), strtolower($_POST['address']), $classId);
+                    $teacherDetails = $this->DbConTeacherLoader->getTeacherById($teacherId);
+                    $tescherStudents = $this->DbConTeacherLoader->getStudentsForTeacher($teacherId);
+                    $tescherClasses = $this->DbConTeacherLoader->getClassesForTeacher($teacherId);
+                    include 'View/teacher/teacherDetailView.php';
+                    break;
+                default:
+                    $teacherDataArr = $this->DbConTeacherLoader->getAllTeachers();
+                    include_once 'View/teacherView.php';
+                    break;
+
             }
         }
     }
 }
+/*TODO
+ * here call getteacherByid
+ * then call getll student data for that teacher from loader method call save to array and display
+ * similar to classes
+ * remove the array things from teacher loader
+ *
+ */
