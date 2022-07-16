147ed8f1596631f9a9ca1a4226f074b0446d30e6
Snehal Asodekar <snehalchetanasodekar@gmail.com>
2022-07-16 13:25:40 +0200

    add html structure to display clean view

---
+++ View/teacher/teacherUpdateView.php
@@ -0,0 +1,81 @@
+
+<!-- *************** -->
+<section class="vh-100 bg-image">
+    <div class="mask d-flex align-items-center h-100 gradient-custom-3">
+        <div class="container h-100">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col-12 col-md-9 col-lg-7 col-xl-6">
+                    <div class="card" style="border-radius: 15px;">
+                        <div class="card-body p-5">
+                            <h2 class="text-uppercase text-center mb-5">Create an account</h2>
+                            <form method="post">
+                                <div class="row">
+                                    <div class="col-md-6 mb-4">
+                                        <div class="form-outline">
+                                            <input type="text" name="first_name"
+                                                   class="form-control form-control-lg" required/>
+                                            <label class="form-label" for="first_name">First name</label>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6 mb-4">
+                                        <div class="form-outline">
+                                            <input type="text" name="last_name"
+                                                   class="form-control form-control-lg" required/>
+                                            <label class="form-label" for="last_name">Last name</label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="form-outline mb-4">
+                                        <input type="email" class="form-control form-control-lg" name="email"  required/>
+                                        <label class="form-label" for="email">Your Email</label>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="form-outline mb-4">
+                                        <input name="address" class="form-control form-control-lg" type="text" required/>
+                                        <label class="form-label" for="address">Address</label>
+                                    </div>
+                                </div>
+                                <!-- Assign class from available classes -->
+                                <div class="form-group pb-3 mb-3">
+                                    <label class="col-md-4 control-label">Assign Class Room</label>
+                                    <div class="col-md-4 selectContainer">
+                                        <div class="input-group">
+                                            <span class="input-group-addon"><i class="glyphicon glyphicon-list"></i></span>
+                                            <select name="classroom" class="form-control selectpicker">
+                                                <?php foreach ($classData as $class) { ?>
+                                                    <option value="<?= $class['id'] ?>">
+                                                        <?= $class['name']; ?>
+                                                    </option>
+                                                <?PHP } ?>
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="d-flex justify-content-center ">
+                                    <button type="button" class="btn border-warning btn-lg">Reset all</button> &nbsp;&nbsp;
+                                    <button type="submit"
+                                            name="page" value="newTeacher"
+                                            class="btn btn-success btn-block btn-lg gradient-custom-4 text-body">
+                                        Register New Teacher
+                                    </button>
+                                </div>
+
+                            </form>
+                            <hr/>
+                            <form method="post">
+                                <div>
+                                    <button type="submit" name="page" class="btn border-success"
+                                            value="detailReturn">Go Back
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
--- View/teacherView.php
+++ View/teacherView.php
@@ -1,34 +1,57 @@
-<table class="table">
-    <thead>
-    <tr>
-        <th scope="col">id</th>
-        <th scope="col">First Name</th>
-        <th scope="col">Last Name</th>
-        <th scope="col">More options</th>
-    </tr>
-    </thead>
-    <tbody>
-    <form method="post">
+<section class="vh-100 bg-image">
+    <div class="mask d-flex align-items-center h-100 gradient-custom-3">
+        <div class="container h-100">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col-12 col-md-9 col-lg-7 col-xl-6">
+                    <div class="card" style="border-radius: 15px;">
+                        <div class="card-body p-5">
+                            <table class="table">
+                                <thead>
+                                <tr>
+                                    <th scope="col">id</th>
+                                    <th scope="col">First Name</th>
+                                    <th scope="col">Last Name</th>
+                                    <th scope="col">Actions</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr class="float :right;">
+                                    <form method="post">
+                                        <button type="submit" name="page" class="btn border-warning m-4 btn-lg gradient-custom-4"
+                                                value="createTeacher">
+                                            Create New Teacher
+                                        </button>
+                                    </form>
+                                </tr>
+                                <?php foreach ($teacherDataArr as $teacher): ?>
+                                    <form method="post">
+                                        <tr>
+                                            <th scope="col"><?= $teacher->getId(); ?></th>
+                                            <th scope="col"><?= $teacher->getFirstName(); ?></th>
+                                            <th scope="col"><?= $teacher->getLastName(); ?></th>
+                                            <th scope="col">
+                                                <div class="btn-group" role="group" aria-label="mixed style">
+                                                    <input type="hidden" name="teacherIdHidden"
+                                                           value="<?= $teacher->getId() ?>"/>
+                                                    <button type="submit" name="page" class="btn border-danger btn-lg"
+                                                            value="deleteTeacher">Delete
+                                                    </button> &nbsp;&nbsp;
+                                                    <button type="submit" name="page" class="btn border-success btn-lg"
+                                                            value="detailTeacher">More Detail
+                                                    </button>
+                                                </div>
 
-        <button type="submit" name="create" class="btn btn-warning"
-                                value="create">Create</button>
-        <?php foreach ($teacherDataArr as $teacher):
-            /* echo "<pre/>";
-          print_r($teacher);exit; */?>
-            <tr>
-                <th scope="col"><?= $teacher->getId(); ?></th>
-                <th scope="col"><?= $teacher->getFirstName(); ?></th>
-                <th scope="col"><?= $teacher->getLastName(); ?></th>
-                <th scope="col">
-                    <div class="btn-group" role="group" aria-label="mixed style">
-                        <button type="submit" name="delete" class="btn btn-danger"
-                                value="<?= $teacher->getId() ?>">Delete</button>
-                        <button type="submit" name="detail" class="btn btn-success"
-                                value="<?= $teacher->getId() ?>">More Detail</button>
+                                            </th>
+                                        </tr>
+                                    </form>
+                                <?php endforeach; ?>
+
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
-                </th>
-            </tr>
-        <?php endforeach;?>
-    </form>
-    </tbody>
-</table>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
