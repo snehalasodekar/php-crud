4c8f1a25db054e1266d7bdf7361cbde7a059ec32
Hub-Mo <Mohamed.elja@hotmail.com>
2022-07-18 14:29:17 +0200

    added a students to controller

--- Controller/ClassRoomController.php
+++ Controller/ClassRoomController.php
@@ -12,12 +12,22 @@ class ClassRoomController{
     public function render(){
 
         $allClassesArray = $this->classRoomLoader->retrieveAllClassRooms();
-        if(isset($_POST['info'])){
-        $id = (int) $_POST['info'];
-        $Students = $this->classRoomLoader->retrieveAllStudents($id);
-        include_once 'View/classesDetailView.php';
-        }else {
-            include_once'View/ClassesView.php';
+        if(isset($_POST['info'])) {
+            $id = (int)$_POST['info'];
+            $Students = $this->classRoomLoader->retrieveAllStudents($id);
+            include_once 'View/classes/classesDetailView.php';
+
+        }elseif(isset($_POST['edit']))
+        {
+            $id = (int)$_POST['edit'] - 1;
+            //$newStudentFirstName = strtolower($_POST['newStudentFirstName']);
+            //$newTeacherLastName = strtolower($_POST['newTeacherFirstName']);
+            $allClassesArray = $this->classRoomLoader->retrieveAllClassRooms()[$id];
+           $editing = $this->classRoomLoader->editClasses($id);
+            include_once 'View/classes/classesEditForm.php';
+        }
+        else{
+            include_once 'View/ClassesView.php';
         }
 
 
