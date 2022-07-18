56c57615a29322b7e129d8513a24901464e1deda
shivamtg <shivamtg1376@gmail.com>
2022-07-18 11:21:00 +0200

    apply styling to studentView

--- View/studentView.php
+++ View/studentView.php
@@ -1,35 +1,68 @@
-<form action="" method="post">
-    <button class="btn btn-success" name="addStudent">Add New Student</button>
-</form>
-<table class="table">
-    <thead>
-    <tr>
-        <th scope="col">id</th>
-        <th scope="col">First Name</th>
-        <th scope="col">Last Name</th>
-        <th scope="col">More options</th>
-    </tr>
-    </thead>
-    <tbody>
-    <form method="post">
-    <?php foreach ($studentArr as $student): ?>
-    <tr>
-        <th scope="col"><?= $student->getId() ?></th>
-        <th scope="col"><?= $student->getName() ?></th>
-        <th scope="col"><?= $student->getLastName() ?></th>
-        <th scope="col">
-            <div class="btn-group" role="group" aria-label="mixed style">
-                <button type="submit" name="delete" class="btn btn-danger"
-                        value="<?= $student->getId() ?>">Delete</button>
-                <button type="submit" name="detail" class="btn btn-success"
-                        value="<?= $student->getId() ?>">More Detail</button>
+
+<section class="vh-100 bg-image">
+    <div class="mask d-flex align-items-center h-100 gradient-custom-3">
+        <div class="container h-100">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col-12 col-md-9 col-lg-7 col-xl-6">
+                    <div class="card" style="border-radius: 15px;">
+                        <div class="card-body p-4">
+                            <h2 class="text-uppercase text-center mb-5">Student View</h2>
+                            <table class="table">
+                                <thead>
+                                <tr>
+                                    <th scope="col">id</th>
+                                    <th scope="col">First Name</th>
+                                    <th scope="col">Last Name</th>
+                                    <th scope="col">Actions</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr class="float :right;">
+                                    <form method="post">
+                                        <button type="submit" name="addStudent"
+                                                class="btn border-warning btn-lg gradient-custom-4">
+                                            Create New Student
+                                        </button>
+                                    </form>
+                                </tr>
+                                <?php foreach ($studentArr as $student): ?>
+                                    <form method="post">
+                                        <tr>
+                                            <th scope="col"><?= $student->getId(); ?></th>
+                                            <th scope="col"><?= $student->getName(); ?></th>
+                                            <th scope="col"><?= $student->getLastName(); ?></th>
+                                            <th scope="col">
+                                                <div class="btn-group" role="group" aria-label="mixed style">
+                                                    <button type="submit" name="delete" class="btn border-danger"
+                                                            value="<?= $student->getId() ?>">Delete
+                                                    </button> &nbsp;&nbsp;
+                                                    <button type="submit" name="detail" class="btn border-success"
+                                                            value="<?= $student->getId() ?>">More Detail
+                                                    </button>
+                                                </div>
+                                            </th>
+                                        </tr>
+                                    </form>
+                                <?php endforeach; ?>
+
+                                </tbody>
+                                <hr/>
+                                <tfoot>
+                                <td>
+                                    <form method="post">
+                                        <button type="submit" name="back"
+                                                class="btn border-warning"
+                                                value="back">
+                                            Home
+                                        </button>
+                                    </form>
+                                </td>
+                                </tfoot>
+                            </table>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </th>
-    </tr>
-    <?php endforeach;?>
-    </form>
-    </tbody>
-</table>
-<form action="" method="post">
-    <button class="btn btn-info" name="back" value="back">Back</button>
-</form>
+        </div>
+    </div>
+</section>
