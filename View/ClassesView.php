0e57e36ed85aa63e3c4f9d630eec13d02e5267c5
Hub-Mo <Mohamed.elja@hotmail.com>
2022-07-18 11:41:05 +0200

    added echo for the value of the buttons

--- View/ClassesView.php
+++ View/ClassesView.php
@@ -1,10 +1,9 @@
 
-<form method="post">
-    <p>test OK</p>
+<form method="post" name="buttons">
     <?php foreach($allClassesArray as $classRoom): ?>
-    <?php echo $classRoom->getId(). " " .$classRoom->getName(). " " .$classRoom->getLocation(). " " .$classRoom->getTeachers(). " " .$classRoom->getStudents(); ?>
-        <button type="submit" name="info" value="<?php $classRoom->getId() ?>">show class info</button>
-        <button type="submit" name="edit" value="<?php $classRoom->getId() ?>">Edit</button>
-        <button type="submit" name="delete" value="<?php $classRoom->getId() ?>">Delete</button><br/>
+    <?php echo $classRoom->getId(). " " .$classRoom->getName(). " " .$classRoom->getLocation(); ?>
+        <button type="submit" name="create" value="<?php echo $classRoom->getId() ?>">CREATE</button>
+        <button type="submit" name="info" value="<?php echo $classRoom->getId() ?>">info</button>
+        <button type="submit" name="delete" value="<?php echo $classRoom->getId() ?>">Delete</button><br/>
     <?php endforeach; ?>
 </form>
