3545ad8075ef4a87be878e15413761da8d54323b
Hub-Mo <Mohamed.elja@hotmail.com>
2022-07-18 11:41:35 +0200

    added the right info to the detail view

--- View/classesDetailView.php
+++ View/classesDetailView.php
@@ -1,2 +1,9 @@
-<?php
 
+
+<?php foreach($allClassesArray as $classRoom): ?>
+    <?php if ($classRoom->getId() == $_POST['info']): ?>
+        <?php echo $classRoom->getId(). " " .$classRoom->getName(). " " .$classRoom->getLocation(). " " . $classRoom->getTeachers(). " ";?>
+        <?php echo implode(", ", $Students); ?>
+        <button type="submit" name="edit" value="<?php echo $classRoom->getId() ?>">Edit</button>
+    <?php endif; ?>
+<?php endforeach; ?>
