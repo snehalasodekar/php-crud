f38a132524b2df2a3c4d310757fb2d4e4fd26087
shivamtg <shivamtg1376@gmail.com>
2022-07-18 10:03:01 +0200

    edit select tag in edit student view

--- View/editStudentView.php
+++ View/editStudentView.php
@@ -2,24 +2,23 @@
     <div class="row">
         <div class="col">
             <label for="firstName">First Name</label>
-            <input name="firstName" id="fisrtName" type="text" class="form-control" value="<?= $foundStudent->getName() ?>">
+            <input required name="firstName" id="fisrtName" type="text" class="form-control" value="<?= $foundStudent->getName() ?>">
         </div>
         <div class="col">
             <label for="lastName">Last Name</label>
-            <input name="lastName" id="lastName" type="text" class="form-control" value="<?= $foundStudent->getLastName() ?>">
+            <input required name="lastName" id="lastName" type="text" class="form-control" value="<?= $foundStudent->getLastName() ?>">
         </div>
         <div class="col">
             <label for="email">Email</label>
-            <input name="email" id="email" type="text" class="form-control" value="<?= $foundStudent->getEmail() ?>">
+            <input required name="email" id="email" type="text" class="form-control" value="<?= $foundStudent->getEmail() ?>">
         </div>
         <div class="col">
             <label for="address">Address</label>
-            <input name="address" id="address" type="text" class="form-control" value="<?= $foundStudent->getAddress() ?>">
+            <input required name="address" id="address" type="text" class="form-control" value="<?= $foundStudent->getAddress() ?>">
         </div>
         <div class="col">
             <label class="mr-sm-2" for="inlineFormCustomSelect">Choose the Class</label>
-            <select class="custom-select mr-sm-2" name="selectClass">
-                <option selected>Choose student class</option>
+            <select required class="custom-select mr-sm-2" name="selectClass">
                 <?php foreach ($classes as $class): ?>
                     <option value="<?= $class['id'] ?>"><?= $class['name'] ?></option>
                 <?php endforeach; ?>
